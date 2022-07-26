18b29a32c608182515804abf4f161d013e1f922c
oleholei <kolomoyets473@gmail.com>
2022-07-26 21:14:03 +0300

    added local notification for place-creating

--- App.tsx
+++ App.tsx
@@ -1,10 +1,39 @@
 import {Fragment, useEffect, useState} from "react";
 import {StatusBar} from 'expo-status-bar';
 import AppLoading from "expo-app-loading";
+import * as Notifications from 'expo-notifications';
 
 import Navigation from "./components/navigation";
 
 import Database from './db';
+
+Notifications.setNotificationHandler({
+	handleNotification: async () => {
+		return {
+			shouldPlaySound: false,
+			shouldShowAlert: true,
+			shouldSetBadge: false
+		}
+	}
+});
+
+const allowsNotificationsAsync = async () => {
+	const settings = await Notifications.getPermissionsAsync();
+	return (
+		settings.granted || settings.ios?.status === Notifications.IosAuthorizationStatus.PROVISIONAL
+	);
+}
+
+const requestPermissionsAsync = async () => {
+	return await Notifications.requestPermissionsAsync({
+		ios: {
+			allowAlert: true,
+			allowBadge: true,
+			allowSound: true,
+			allowAnnouncements: true,
+		},
+	});
+}
 
 export default function App() {
 	const [dbInitialized, setDbInitialized] = useState(false);
@@ -13,7 +42,21 @@ export default function App() {
 		Database.init().then(() => {
 			setDbInitialized(true);
 		});
+		
+		if (!allowsNotificationsAsync()) {
+			requestPermissionsAsync();
+		}
+		
+		const submission = Notifications.addNotificationReceivedListener((notification) => {
+			console.log(notification);
+		});
+		
+		return () => {
+			submission.remove();
+		}
 	}, []);
+	
+
 	
 	if (!dbInitialized) return <AppLoading/>;
 	
--- screens/add-place/index.tsx
+++ screens/add-place/index.tsx
@@ -1,5 +1,6 @@
 import {FC} from 'react';
 import {View, Text} from "react-native";
+import * as Notifications from 'expo-notifications';
 
 import PlacesService from "../../db/places";
 
@@ -9,10 +10,25 @@ import Place from "../../utils/types/places";
 import PlaceForm from "../../components/layout/places-form";
 
 const AddPlace: FC<StackScreen<StackScreens.addPlace>> = ({navigation}) => {
+	const scheduleNotification = () => {
+		return Notifications.scheduleNotificationAsync({
+			content: {
+				title: 'Place was added',
+				body: 'You can see this details on All-Places page'
+			},
+			trigger: {
+				seconds: 5
+			}
+		});
+	};
+	
 	const onPlaceAdd = async (place: Place) => {
-		await PlacesService.addPlace(place).then(() => {
+		await scheduleNotification();
+		
+		PlacesService.addPlace(place).then(() => {
 			navigation.navigate(StackScreens.all, {place});
 		});
+		
 	}
 	
 	return (
